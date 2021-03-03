2d750fdf1a692d843f359f5e2444af7af6c87193
Trent Shailer <trent.shailer@gmail.com>
2021-03-03 16:39:36 +1300

    Basic front-end routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
 import "rsuite/dist/styles/rsuite-dark.css";
 import { Button } from "rsuite";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
 	return (
-		<div className="App">
-			<Button appearance="primary"> Hello world </Button>
-		</div>
+		<Router>
+			<div className="App">
+					<Button appearance="primary"> Hello world </Button>
+				<Switch>
+					<Route path="/about">
+						<Test />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
+}
+
+function Test() {
+	return (<h2>Test</h2>)
 }
 
 export default App;
