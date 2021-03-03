8f2d15c0cbbbf47ebe5b360440f54ffaa4b95973
Trent Shailer <trent.shailer@gmail.com>
2021-03-03 16:22:17 +1300

    Basic React Suite with webserver

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "rsuite/dist/styles/rsuite-dark.css";
+import { Button } from "rsuite";
+
+function App() {
+	return (
+		<div className="App">
+			<Button appearance="primary"> Hello world </Button>
+		</div>
+	);
+}
+
+export default App;
